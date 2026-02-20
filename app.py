337f4f2085a9d94e5d15622f33e64ad51572b401
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-20 13:36:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ MIN_NUM = 1
 MAX_NUM = 100
 MAX_GUESSES = 7
 
-password = "Ghbfw_oifaGkBbf7646Ff@jg!"
+DB_PASSWORD = "Ghbfw_oifaGkBbf7646Ff@jg!"
 # Initialize the secret number and number of guesses in session state
 if 'secret_number' not in st.session_state:
     st.session_state.secret_number = random.randint(MIN_NUM, MAX_NUM)
