660bedba4ea7234dc4963e14ce852eec54ee99f8
sockeroc <sockeroc@outlook.com>
2026-02-24 11:18:44 -0500

    vuln introduced for gate

---
+++ date.js
@@ -0,0 +1,17 @@
+function changedate() {
+	document.getElementById('Today').innerHTML= Date();
+}
+
+/* use the below to load once a page loads
+<script>
+  window.addEventListener("load", () => {
+    console.log("This function is executed once the page is fully loaded");
+  });
+</script>
+*/
+
+/* The below is a test for SNYK SAST Scans
+*/
+const database_password = "p@ssword12345!";
+const dbURI = "mongodb://admin:fake_password_99@localhost:27017/mydb";
+const github_token = "ghp_zXy123abc456def789ghiJKLmnoPQRstuVWX";
